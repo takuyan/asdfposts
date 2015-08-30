050bd85d672f6bc5f41a9878153ddb1b083863f0
Takuya Kato <github@takuyan.com>
2015-08-30 12:43:36 +0900

    add create and show

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -3,4 +3,25 @@ class Api::V1::PostsController < Api::V1::BaseController
     @posts = Post.all
     respond_with @posts
   end
+
+  def create
+    @post = Post.new post_params
+    @post.user = current_user
+    if @post.save
+      respond_with @post, status: 200, location: api_v1_post_url(@post)
+    else
+      render json: @post.errors, status: 403
+    end
+  end
+
+  def show
+    @post = Post.find params[:id]
+    respond_with @post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :body)
+  end
 end
